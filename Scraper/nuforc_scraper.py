c81a5e9dcd10718029b27c29110d748816d83d90
Lincoln Wentz <linkwentz@gmail.com>
2021-06-22 22:41:45 -0500

    Make basic scraper loop

--- Scraper/nuforc_scraper.py
+++ Scraper/nuforc_scraper.py
@@ -1,4 +1,5 @@
 from urllib.request import Request, urlopen
+import re
 
 from bs4 import BeautifulSoup
 import numpy as np
@@ -18,3 +19,58 @@ for tr in homepage.find('tbody').findAll('tr'):
     urls.append(url)
 
 del urls[-1]
+
+def grab_row_data(row):
+    row = row.findAll('td')
+    series = pd.Series()
+    timestamp = re.split('[:/ ]', row[0].text)
+    timestamp = list(map(lambda x : '0{}'.format(x) if len(x) == 1 else x, timestamp))
+    year = timestamp[0]
+    month = timestamp[1]
+    day = timestamp[2]
+    hour = timestamp[3]
+    minute = timestamp[4]
+    second = '00'
+    # Generate timestamp
+    ts = '{}-{}-{}T{}:{}:{}Z'
+    series['event_time'] = ts.format(year, month, day, hour, minute, second)
+    # Generate datestamp
+    ds = '{}-{}-{}'
+    series['event_date'] = ds.format(year, month, day)
+    # Add individual time components
+    series['year'] = year
+    series['month'] = month
+    series['day'] = day
+    series['hour'] = hour
+    series['minute'] = minute
+    series['second'] = second
+    # city
+    series['city'] = row[1].text
+    # state
+    series['state'] = row[2].text
+    # shape
+    series['shape'] = row[3].text
+    # duration
+    series['duration'] = row[4].text
+    # summary
+    series['summary'] = row[5].text
+    # event_url
+    series['event_url'] = row[0].find('font').find('a')['href']
+    
+    return series
+
+def grab_table_from_url(url):
+    request = Request(url, 
+              headers = {'User-Agent':'Mozilla/5.0'})
+    page = BeautifulSoup(urlopen(request).read(), 'html.parser')
+    table = page.find('tbody').findAll('tr')
+    
+    rows = list(map(grab_row_data, row))
+    
+    frame = pd.DataFrame(rows)
+
+def 
+    
+frames = list(map(grab_table_from_url, urls))
+data = pd.concat(frames,
+                 ignore_index = True)
